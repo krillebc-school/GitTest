0ce9d9035a57b0258be69b48e6fdaa247d29e9b5
christian chekerdemian <eaa5cbc@students.eaaa.dk>
2025-08-20 16:10:58 +0200

    update helloworld

--- src/app/helloWorld.java
+++ src/app/helloWorld.java
@@ -3,5 +3,6 @@ package app;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hello again world!");
     }
 }
