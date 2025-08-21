a6e1784a00f0ba933d15f3729c875115126a61a9
krillebc-school <eaa25cbc@students.eaaa.dk>
2025-08-21 09:57:12 +0200

    second update to helloWorld

--- src/app/helloWorld.java
+++ src/app/helloWorld.java
@@ -4,5 +4,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello again world!");
+        System.out.println("Hello once again world!");
     }
 }
